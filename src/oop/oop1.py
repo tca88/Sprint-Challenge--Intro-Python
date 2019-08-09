33c3b2ce44d19127414635a3c1018734cdb43697
tca88 <trishnaamin@gmail.com>
2019-08-09 12:16:02 -0400

    Complete oop1 to create all of the classes, and highlight which is the base class and which are subclasses. Ran test_oop1.py to make sure everything ran and passed as expected.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,45 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#VEHICLE IS A BASE/PARENT CLASS
+class Vehicle:
+    def __init__(self):
+        pass
+#############
+
+#FLIGHTVEHICLE IS A SUBCLASS/CHILD CLASS of VEHICLE
+class FlightVehicle(Vehicle): # "is-a" relationship
+    def __init__(self):
+        pass
+#############
+
+#GROUNDVEHICLE IS A SUBCLASS/CHILD CLASS of VEHICLE
+class GroundVehicle(Vehicle): # "is-a" relationship
+    def __init__(self):
+        pass
+#############
+
+#STARSHIP IS A SUBCLASS/CHILD CLASS of FLIGHTVEHICLE
+class Starship(FlightVehicle): # "is-a" relationship
+    def __init__(self):
+        pass
+#############
+
+#AIRPLANE IS A SUBCLASS/CHILD CLASS of FLIGHTVEHICLE
+class Airplane(FlightVehicle): # "is-a" relationship
+    def __init__(self):
+        pass
+#############
+
+#CAR IS A SUBCLASS/CHILD CLASS of GROUNDVEHICLE
+class Car(GroundVehicle): # "is-a" relationship
+    def __init__(self):
+        pass
+#############
+
+#MOTORCYCLE IS A SUBCLASS/CHILD CLASS of GROUNDVEHICLE
+class Motorcycle(GroundVehicle): # "is-a" relationship
+    def __init__(self):
+        pass
+#############
